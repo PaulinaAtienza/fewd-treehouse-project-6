dd2f88b6a5c3a3347b1d5519abc07083723aa2d2
Paulina Atienza <PaulinaAtienza@users.noreply.github.com>
2020-10-29 10:45:30 -0500

    Add some variables and event listener code to javascript

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const startButton = document.getElementsByClassName('btn__reset');
+const overlay = document.getElementById('overlay');
 let missed = 0;
+
+const phrasesArray = [
+    'I love JavaScript',
+    'Keep your code DRY',
+    '',
+    '',
+    ''
+]
+
+overlay.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+        overlay.style.display = 'none';
+    }
+});
+
