f86b8f69ce58e87cc9555343f15eec631208b3dd
Paulina Atienza <PaulinaAtienza@users.noreply.github.com>
2020-10-30 15:56:01 -0500

    Add phrase and display functions to javascript

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const ul = phrase.querySelector('ul')
 const startButton = document.getElementsByClassName('btn__reset');
 const overlay = document.getElementById('overlay');
+const heading = document.getElementById('banner');
 let missed = 0;
 
-const phrasesArray = [
-    'I love JavaScript',
-    'Keep your code DRY',
-    '',
-    '',
-    ''
+const phrases = [
+    ['Google Chrome, Safari and Firefox are types of what?', 'Web browsers'],
+    ['What phrase describes best practice when coding?', 'Keep your code DRY'],
+    ['What programming language allows you to add interactivity to a web page?', 'JavaScript'],
+    ['HTML stands for?', 'HyperText Markup Language'],
+    ['Where is the best place to learn front-end web development?', 'Treehouse']
 ]
+
+const questionNumber = Math.floor(Math.random() * phrases.length);
 
 // Start game button listener
 overlay.addEventListener('click', (e) => {
@@ -19,15 +23,38 @@ overlay.addEventListener('click', (e) => {
     }
 });
 
+// Choose random array and display question
+function getRandomPhraseAsQuestion(arr) {
+    const question = arr[questionNumber][0];
+    const questionHeading = document.createElement('h3');
+    questionHeading.textContent = question;
+    heading.append(questionHeading);
+}
+
 // Choose random array and split into array of letters
 function getRandomPhraseAsArray(arr) {
-
+    const splitPhrase = arr[questionNumber][1].split('');
+    return splitPhrase;
 }
 
 // Display the random phrase
 function addPhraseToDisplay(arr) {
-
+    const phrasesArray = getRandomPhraseAsArray(arr);
+    for (let i = 0; i < phrasesArray.length; i+= 1) {
+        let character = phrasesArray[i];
+        const li = document.createElement('li');
+        li.textContent = character;
+        ul.append(li);
+        if (character !== ' ') {
+            li.className = 'letter';
+        } else {
+            li.className = 'space';
+        }
+    }
 }
+
+getRandomPhraseAsQuestion(phrases);
+addPhraseToDisplay(phrases);
 
 // Check letters to random phrase chosen
 function checkLetter(button) {
